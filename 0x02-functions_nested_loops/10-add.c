e1e0a0743d5a497eb3c99c591844e361337d05d7
abuhviktor <abuhviktor@gmail.com>
2023-02-21 10:54:31 +0100

    tables fibonacci sequence

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ *
+ * @num1: number one.
+ * @num2: number two.
+ *
+ * Return: Add of the two numbers.
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
